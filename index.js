6b29b1c5f7d08c901889e135f3fd4af41bce4b54
IK62 <ismail.karrouch13@gmail.com>
2022-11-17 10:12:20 +0100

    work as intended

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // Project
+//use axios
 const axios = require('axios')
-const cities = require('all-the-cities')
-
-city = cities.filter(city => city.name === (process.argv[2]))
-sortedCity = city.sort((a, b) => b.population - a.population)
-filtredCity = sortedCity[0]
-coords = filtredCity.loc.coordinates
-const request = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${coords[0]}&lon=${coords[1]}&appid=6edefaabdbd92cd6dec9908ffa90d25b`)
+//get data from open weather map
+const request = axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${process.argv[2]}&appid=6edefaabdbd92cd6dec9908ffa90d25b`)
+//use the data
 request.then(({data: {main}}) => {
+    //convert the temp from kelvin to celsius
     let temperature = main.temp - 273.15
+    //round the temp
     temperature = Math.round(temperature)
+    //useless humidity variable
     let humidity = main.humidity
-    console.log(`il fais ${temperature}°C à ${filtredCity.name}`)
+    //log the requested response
+    console.log(`il fais ${temperature}°C à ${process.argv[2]}`)
 })
